9db31664e4477f4e4d872f59df0cb7604adfefeb
Yitbarek Wendimu <72982296+yitbehisbro@users.noreply.github.com>
2022-11-24 09:04:46 +0300

    Updates

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Module of Unittests """
+""" Unittests module for base_model.py """
 import unittest
 from models.base_model import BaseModel
 import os
@@ -8,7 +8,7 @@ from models.engine.file_storage import FileStorage
 import datetime
 
 
-class BaseModelTests(unittest.TestCase):
+class TestBaseModel(unittest.TestCase):
     """ Suite of Console Tests """
 
     my_model = BaseModel()
