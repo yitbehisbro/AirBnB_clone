608093d4d61e38447c2b1d81b88e6528e328fc39
Yitbarek Wendimu <72982296+yitbehisbro@users.noreply.github.com>
2022-11-24 08:48:00 +0300

    Updates

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-""" Import modules and packages """
+""" create a unique FileStorage instance
+for the application """
 from models.engine.file_storage import FileStorage
 
 storage = FileStorage()
