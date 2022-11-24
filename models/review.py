db63441b9d9355e4d56cb9ee8f61361a1fbb92ff
Yitbarek Wendimu <72982296+yitbehisbro@users.noreply.github.com>
2022-11-24 08:56:09 +0300

    Updates

--- models/review.py
+++ models/review.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-"""
-Defines review class
-"""
+""" Review module """
 from models.base_model import BaseModel
 
 
 class Review(BaseModel):
-    """Reviews made by users about a place"""
+    """Creates attribute for reviews made by users
+    about a place
+    """
     place_id = ""
     user_id = ""
     text = ""
